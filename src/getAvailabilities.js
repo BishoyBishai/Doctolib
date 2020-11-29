370a06f6bd7f694b5144339e951b492879468c4c
Doctolib <>
2020-11-29 16:41:28 +0100

    Create getEventsList to return eventList Use CONSTANT to access event's kinds Create getEventsList to get initial state of availabilities

--- src/getAvailabilities.js
+++ src/getAvailabilities.js
@@ -1,7 +1,22 @@
 import moment from "moment";
 import knex from "knexClient";
 
-export default async function getAvailabilities(date) {
+/*===================[EVENT KINDS]====================*/
+const OPEN_EVENT = "opening";
+const APPOINTMENT_EVENT = "appointment";
+
+/*===================[getEventsList]====================*/
+const getEventsList = async (date) => {
+  return await knex
+    .select("kind", "starts_at", "ends_at", "weekly_recurring")
+    .from("events")
+    .where(function() {
+      this.where("weekly_recurring", true).orWhere("ends_at", ">", +date);
+    });
+};
+
+/*===================[getInitialAvailabilities]====================*/
+const getInitAvailabilities = (date) => {
   const availabilities = new Map();
   for (let i = 0; i < 7; ++i) {
     const tmpDate = moment(date).add(i, "days");
@@ -10,13 +25,14 @@ export default async function getAvailabilities(date) {
       slots: []
     });
   }
+  return availabilities;
+};
 
-  const events = await knex
-    .select("kind", "starts_at", "ends_at", "weekly_recurring")
-    .from("events")
-    .where(function() {
-      this.where("weekly_recurring", true).orWhere("ends_at", ">", +date);
-    });
+/*===================[getAvailabilities]====================*/
+
+export default async function getAvailabilities(date) {
+  const availabilities = getInitAvailabilities(date);
+  const events = await getEventsList(date);
 
   for (const event of events) {
     for (
@@ -25,9 +41,9 @@ export default async function getAvailabilities(date) {
       date.add(30, "minutes")
     ) {
       const day = availabilities.get(date.format("d"));
-      if (event.kind === "opening") {
+      if (event.kind === OPEN_EVENT) {
         day.slots.push(date.format("H:mm"));
-      } else if (event.kind === "appointment") {
+      } else if (event.kind === APPOINTMENT_EVENT) {
         day.slots = day.slots.filter(
           slot => slot.indexOf(date.format("H:mm")) === -1
         );
